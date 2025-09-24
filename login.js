92b08b69949b80c5283e4016cbc436fc59848651
bacaymayjheleann1996-maker <bacaymayjheleann1996@gmail.com>
2025-09-24 13:53:18 +0800

    login

--- login.js
+++ login.js
@@ -1,6 +1,20 @@
 function login(){
-    let username=document.getElementById("user name").value;
+    //Email:mhaymhay@gmail.com // Error Message: Your Emailis incorrect
+    //Password: 12342 // Err
+    let username=document.getElementById("User Name").value;
+    let Passwrod=document.getElementById("Password").value;
 
 
-    console.log("you enter:"+ ""+ username);
+
+    console.log("you enter:"+" "+ UserName);
+    console.log("you enter:"+" "+ Password);
+
+
+if(Email==="mhaymhay@gmail.com"){
+    console.log("tama ang iyong email")
+}else{
+    
+    console.log("mali amg iyong email")
+}
+
 }
