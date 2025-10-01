05b0eb7a89a0249d5bc340742549b83de82c0a4a
bacaymayjheleann1996-maker <bacaymayjheleann1996@gmail.com>
2025-10-01 12:32:50 +0800

    checked

--- login.js
+++ login.js
@@ -10,11 +10,25 @@ function login(){
     console.log("you enter:"+" "+ Password);
 
 
-// if(Email==="mhaymhay@gmail.com"){
-//     console.log("tama ang iyong email")
-// }else{
-    
-//     console.log("mali amg iyong email")
-// }
+if(login==="mhay"){
 
+ console.log("tama ang iyong username")
+}else{
+    
+    
+     console.log("mali amg iyong username")
+
+ }
+ if (Password==="hello"){
+    console.log("tama ang iyong password")
+ }
+ else("mali ang pasword")
+ 
+ if(login==="mhay" && Password==="hello")
+{console.log("ang iyong email at password")
+}else if(login==="mhay" && Password==="hello"){
+    console.log("ang username at password ay mali")
+
+}
+email
 }
