29979701d34f93611452829b6c67f17ce3d8b157
bacaymayjheleann1996-maker <bacaymayjheleann1996@gmail.com>
2025-10-08 11:53:47 +0800

    hands on exam

---
+++ handsOn1B.js
@@ -0,0 +1,27 @@
+function Compute (){
+
+    let Height=document.getElementById("Height").value;
+    let Weight=document.getElementById("Weight").value;
+
+
+
+    console.log("you enter:"+" "+ Height*Height);
+    console.log("you enter:"+" "+ Weight);
+
+
+if(BMI=18.5){
+
+ console.log("Underweight");
+}
+ if (BMI=18.5 && BMI<=24.9 ){
+    console.log("Normal Weight");
+ }
+ if(BMI=25 && BMI >=29.9)
+ 
+{console.log("Overweight")
+}else if(Height===""){
+    console.log("Please enter you height")
+
+}
+email
+}
